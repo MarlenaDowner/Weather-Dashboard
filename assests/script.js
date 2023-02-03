695a25a31200fceb846763c15a494ae031cba2dd
MarlenaDowner <119687938+MarlenaDowner@users.noreply.github.com>
2023-02-03 19:23:21 +0000

    added starter code

---
+++ assests/script.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+
+
+    
+})
+
+$("button").on("click", function() {})
+
+
+var search = $(this).attr("search-button");
+// alert("i was clicked");
+
+var APIKey = "d649dbc7935a0994373059b2dbf6bed6";
+var queryURL = "https://api.openweathermap.org/data/2.5/weather?" + "q=Bujumbura,Burundi&appid=" + APIKey; 
