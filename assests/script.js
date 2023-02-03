046137b2a881c03f84a6d098d1089d7787f40a1b
MarlenaDowner <119687938+MarlenaDowner@users.noreply.github.com>
2023-02-03 19:43:14 +0000

    added keys, testing is london will be brough up in search, also added ajax

--- assests/script.js
+++ assests/script.js
@@ -1,8 +1,5 @@
-$(document).ready(function() {
-
-
-    
-})
+var APIKey = "d649dbc7935a0994373059b2dbf6bed6";
+var queryURL = "https://api.openweathermap.org/data/2.5/weather?" + "q=London&limit=5&appid=" + APIKey; 
 
 $("button").on("click", function() {})
 
@@ -10,5 +7,3 @@ $("button").on("click", function() {})
 var search = $(this).attr("search-button");
 // alert("i was clicked");
 
-var APIKey = "d649dbc7935a0994373059b2dbf6bed6";
-var queryURL = "https://api.openweathermap.org/data/2.5/weather?" + "q=Bujumbura,Burundi&appid=" + APIKey; 
