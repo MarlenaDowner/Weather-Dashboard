cc02d9d9b77dbbcefb567d2fb55d24e93f63051e
MarlenaDowner <119687938+MarlenaDowner@users.noreply.github.com>
2023-02-03 20:03:05 +0000

    commented out css in html as was stopping code from working, will add back later. also added test code for Bujumbura to check api was working

--- assests/script.js
+++ assests/script.js
@@ -1,9 +1,25 @@
+//API keys
 var APIKey = "d649dbc7935a0994373059b2dbf6bed6";
-var queryURL = "https://api.openweathermap.org/data/2.5/weather?" + "q=London&limit=5&appid=" + APIKey; 
+var queryURL = "https://api.openweathermap.org/data/2.5/weather?" + "q=Bujumbura,Burundi&appid=" + APIKey;
 
+// Event listener for all button elements
 $("button").on("click", function() {})
 
-
+ // In this case, the "this" keyword refers to the "search-button" that was clicked
 var search = $(this).attr("search-button");
-// alert("i was clicked");
 
+//pulling request
+$.ajax({
+    url: queryURL,
+    method: "GET"
+  })
+  
+.then(function(response) {
+
+ // Log the queryURL
+ console.log(queryURL);
+
+ // Log the resulting object
+ console.log(response);
+
+});
