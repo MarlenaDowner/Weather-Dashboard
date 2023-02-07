5433f2edb07dd1ad8e8375bd703bdb376c86d06a
MarlenaDowner <119687938+MarlenaDowner@users.noreply.github.com>
2023-02-07 16:34:02 +0000

    added if statments for if user added a city that is already appended, also added to local storage

--- assests/script.js
+++ assests/script.js
@@ -6,6 +6,7 @@ const WeatherKey = "d649dbc7935a0994373059b2dbf6bed6";
 
 let inputSearch = $("#search-input")
 let inputForm = $("#search-form")
+let inputHistory = []
 
 
 function fetchLocation(search){ //this is grabbing the value fetchLocation(search); at bottom
@@ -17,6 +18,18 @@ $.ajax({
     url: queryURL,
     method: "GET"
   }).then(function(response){
+    if(!response[0]){ //if users input not correct
+      alert("Location not found, try again")
+    }else{
+
+      if(inputHistory.indexOf(search) !== -1) { //if search bu user already appended OR if no search term return th index back
+        return
+      }
+      inputHistory.push(search);  //adding to the search history
+
+      localStorage.setItem("search-history", JSON.stringify(inputHistory));  //adding to local storage
+
+    }
 
  // Log the queryURL
  console.log(queryURL);
