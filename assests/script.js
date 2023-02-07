3725bd8b8599ae62c876ba1a8d6fc7106a572d7a
MarlenaDowner <119687938+MarlenaDowner@users.noreply.github.com>
2023-02-07 11:48:41 +0000

    added api keys

--- assests/script.js
+++ assests/script.js
@@ -1,17 +1,29 @@
- // In this case, the "this" keyword refers to the "search-button" that was clicked
-//  var search = $(this).attr("search-button");
+//API keys
+const WeatherURL = "https://api.openweathermap.org";
+const WeatherKey = "d649dbc7935a0994373059b2dbf6bed6"
+
+
+
 
 //inputs from HTML
 
 let inputSearch = $("#search-input")
 let inputForm = $("#search-form")
 
+
+function fetchLocation(){
+  let 
+
+}
+
 function submitInputForm(event){
 
   event.preventDefault();
+  let search = inputSearch.val().trim();
+
+  fetchLocation(search);
 
 
-  alert(inputSearch.val().trim());
 }
 
 inputForm.on("submit", submitInputForm);
@@ -19,8 +31,6 @@ inputForm.on("submit", submitInputForm);
 
 
 
-//API keys
-const WeatherURL = "https://api.openweathermap.org";
-const WeatherKey = "d649dbc7935a0994373059b2dbf6bed6"
+
 
 
