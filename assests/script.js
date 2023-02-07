c4f9f4222e19d96010290cfd4a1b6dddaaae590e
MarlenaDowner <119687938+MarlenaDowner@users.noreply.github.com>
2023-02-07 13:34:50 +0000

    figured out why api wasnt working, didnt have api at beginning of address. Also figured why ajax wasnt working, had the url as weatherurl when should have been guery url

--- assests/script.js
+++ assests/script.js
@@ -1,9 +1,18 @@
 //API keys
 const WeatherURL = "https://api.openweathermap.org";
-const WeatherKey = "d649dbc7935a0994373059b2dbf6bed6"
+const WeatherKey = "d649dbc7935a0994373059b2dbf6bed6";
 
+// var WeatherURL = "https://api.openweathermap.org" + WeatherKey;
 
+///data/2.5/weather?
 
+//"https://api.openweathermap.org/data/2.5/weather?" + "q=Bujumbura,Burundi&appid=" + APIKey;
+
+//"https://api.openweathermap.org/geo/2.5/direct?" + "q=Bujumbura,Burundi&appid=" + APIKey;
+
+///geo/1.0/direct?
+
+///geo/2.5/direct?
 
 //inputs from HTML
 
@@ -12,9 +21,42 @@ let inputForm = $("#search-form")
 
 
 function fetchLocation(search){ //this is grabbing the value fetchLocation(search); at bottom
-  let queryURL = `${WeatherURL}/geo/1.0/direct?q=£=${search}&limit=5appid=${WeatherKey}`;
+  let queryURL = `${WeatherURL}/geo/1.0/direct?q=${search}&limit=5&appid=${WeatherKey}`;
+  console.log(queryURL);
+
+
+  
+  //pulling request
+$.ajax({
+    url: queryURL,
+    method: "GET"
+  }).then(function(response){
+
+ // Log the queryURL
+ console.log(queryURL);
+
+ // Log the resulting object
+ console.log(response);
+
+
+  }) 
+  
 
 }
+
+
+// .then(function(response) {
+
+
+//  // Log the queryURL
+//  console.log(queryURL);
+
+//  // Log the resulting object
+//  console.log(response);
+
+// });
+
+// }
 
 function submitInputForm(event){
 
@@ -22,7 +64,6 @@ function submitInputForm(event){
   let search = inputSearch.val().trim();
 
   fetchLocation(search);
-
 
 }
 
